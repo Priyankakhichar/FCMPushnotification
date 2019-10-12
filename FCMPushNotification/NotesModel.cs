0c6b6c0a7a8d771106053008685cffbe1e868a8d
Priyankakhichar <priyanka.swettu@gmail.com>
2019-10-12 13:08:01 +0530

    first commit

---
+++ FCMPushNotification/NotesModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCMPushNotification
+{
+    /// <summary>
+    /// notes model class
+    /// </summary>
+    public class NotesModel
+    {
+
+        /// <summary>
+        /// id
+        /// </summary> 
+        public int Id
+        {
+            get;
+            set;
+        }
+
+       
+        public string UserId
+        {
+            get; set;
+        }
+        /// <summary>
+        /// title
+        /// </summary>
+        public string Title
+        {
+            get; set;
+        }
+
+
+        /// <summary>
+        /// description
+        /// </summary>
+        public string Description
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// color
+        /// </summary>
+        public string Color
+        {
+            get; set;
+        }
+
+
+       
+
+        public bool IsPin
+        {
+            get; set;
+        }
+        public DateTime? Reminder
+        {
+            get; set;
+        }
+        public string Image
+        {
+            get; set;
+        }
+        /// <summary>
+        /// created date
+        /// </summary>
+        public DateTime? CreateDate
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// modified date
+        /// </summary>
+        public DateTime? ModifiedDate
+        {
+            get; set;
+        }
+    }
+}
---
+++ FCMPushNotification/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FCMPushNotification
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (var client = new HttpClient())
+            {
+                ////giving the base address of our API and setting the response header.
+                client.BaseAddress = new Uri("https://localhost:44382/api/Notes/sendMessage");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                //HTTP GET method
+                MyAPIGet(client).Wait();
+            }
+        }
+        static async Task MyAPIGet(HttpClient cons)
+        {
+            using (cons)
+            {
+                ////Sends a GET request to the specified Uri as an asynchronous operation.
+                var response = await cons.GetAsync("https://localhost:44382/api/Notes/sendMessage");
+                response.EnsureSuccessStatusCode();
+
+                ////Read the content of the result using ReadAsStringAsync() method.
+                string result = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(result);
+                
+            }
+        }
+    }
+}
